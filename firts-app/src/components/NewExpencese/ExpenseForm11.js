72e1b8054daf49fdf48e1fe023d92a9efe275d1e
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-06-08 08:37:33 +0330

    Daily practice update/designForm/form input

--- firts-app/src/components/NewExpencese/ExpenseForm11.js
+++ firts-app/src/components/NewExpencese/ExpenseForm11.js
@@ -1,42 +1,57 @@
 import { useState } from "react";
 import "./ExpenseForm12.css";
-const ExpenseForm =() =>{
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount]= useState('');
-    const[enteredDate, setenteredDate]= useState('');
+const ExpenseForm =(props) =>{
+    const [enteredTitle, setEnteredTitle] = useState('eine title');
+    const [enteredAmount, setEnteredAmount]= useState("");
+    const[enteredDate, setenteredDate]= useState('2025-05-05');
 
     // define method of handler
-    const titleChangeHandler =()=>{
-        console.log('title change');
-    }
+    const titleChangeHandler =(event)=>{
+        // console.log('title change');
+        setEnteredTitle(event.target.value);
+    };
 
-    const amountchangeHandler =() => {
-        console.log('test amount');
-    }
+    const amountchangeHandler =(event) => {
+        // console.log('test amount');
+        setEnteredAmount(event.target.value);
+    };
 
-    const datechangeHandler =() =>{
-        console.log('test date ');
+    const datechangeHandler =(event) =>{
+         // setenteredDate('test3 date');
+        setenteredDate(event.target.value);
+    };
+    // ein wichtig punkt ist, wir benutsen event fur kein auto refresh
+    const submitHandler=(event) =>{
+        // event . preventDefutl ist fur nicht mache was sie muss zu tun. funyyy=)
+        event.preventDefult();
+        // console.log( 'submit');
+        const expenseDate={
+            title:enteredTitle,
+            amount:enteredAmount,
+            date : new Date (enteredDate)
+        };
+        props.onSaveExpenseDate(expenseDate);
     }
 
   
     // fur form
-    return <form>
+    return <form onSubmit={submitHandler}>
         <div className="new-expense-controls">
             {/* title */}
             <div className="new-expense-control-test">
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                <input type="text" onChange={titleChangeHandler} value={enteredTitle}/>
             </div>
             {/* amount */}
              <div className="new-expense-control-test">
                 <label>Amount</label>
-                <input type="number" min="1" step="10" onChange={amountchangeHandler}/>
+                <input type="number" min="1" step="10" onChange={amountchangeHandler} value={enteredAmount}/>
             </div>
 
             {/* date */}
              <div className="new-expense-control-test">
                 <label>Date</label>
-                <input type="date" min="2023-1-1" onChange={datechangeHandler}/>
+                <input type="date" min="2023-1-1" onChange={datechangeHandler} value={enteredDate}/>
             </div>
             {/* button fur add */}
             <div className="new-expense-action">
--- firts-app/src/components/NewExpencese/NewExpense9.js
+++ firts-app/src/components/NewExpencese/NewExpense9.js
@@ -3,11 +3,13 @@ import Card from "../UI/Card77";
 import ExpenseForm from "./ExpenseForm11";
 
 const NewExpense = () => {
+  const saveExpenseDataHandler = enterdexpeneDate=>{
+    console.log(enterdexpeneDate);
+    console.log("test");
+  }
   return (
     <Card className="new-expense">
-      <ExpenseForm>
-
-      </ExpenseForm>
+      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler}></ExpenseForm>
     </Card>
   );
 };
