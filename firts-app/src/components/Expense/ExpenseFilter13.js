12c8da4ab03204b8844c545d7a994e5cc12f345e
Hsis8hJ39@&@£×92992ajue92024 <parasto.majidi0@gmail.com>
2025-06-10 08:52:54 +0330

    Daily practice update/filter list

---
+++ firts-app/src/components/Expense/ExpenseFilter13.js
@@ -0,0 +1,23 @@
+import './ExpenseFilter14.css';
+
+const ExpenseFilter = props =>{
+    const selectedYearChangeHandler = event=>{
+        props.onSelectedYearChanged(event.target.value);
+    }
+
+return(
+    <div className='expense-Filter'>
+        <div className='expense-filter-control'>
+            <label>year</label>
+            <select value="" onChange={selectedYearChangeHandler}>
+                <option value="2026">2026</option>
+                <option value="2025">2025</option>
+                <option value="2024">2024</option>
+                <option value="2023">2023</option>
+            </select>
+        </div>
+    </div>
+)
+}
+
+export default ExpenseFilter;
--- firts-app/src/components/Expense/Expenses3.js
+++ firts-app/src/components/Expense/Expenses3.js
@@ -1,19 +1,27 @@
 import ExpenseItem from "../Expense/ExpenseItems1";
 import Card from "../UI/Card77";
-import './Expenses4.css';
+import "./Expenses4.css";
+import ExpenseFilter from "./ExpenseFilter13";
+import { useState } from "react";
 function Expenses(props) {
+  const [year, setYear] =useState('2025');
+  const filterChangeHandler =filterValue=>{
+    setYear(filterValue);
+  }
+
   return (
     // expenses form 4 attrebitu
     <Card className="expenses">
-        {/* items define in APP.js */}
-        {props.items.map(expense =>
-       
-          <ExpenseItem 
-           key={expense.id}
+      <ExpenseFilter selected={year} onSelectedYearChanged={filterChangeHandler}/>
+      {/* items define in APP.js */}
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
           title={expense.title}
-          amount ={expense.amount}
-          date={expense.date}/>  
-       )}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
       {/* <ExpenseItem
         title={props.items[0].title}
         price={props.items[0].price}
